4fb4c8d22eb354ddfd422695e15d4a32e47b5011
unknown <44147809+LSabin23@users.noreply.github.com>
2021-12-01 19:17:43 -0500

    added getStats and getInventory methods using prototype syntax

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -2,7 +2,6 @@ const Potion = require('../lib/Potion')
 
 jest.mock('../lib/Potion')
 
-const { expect } = require('@jest/globals')
 const Player = require('../lib/Player')
 
 test('create a player object', () => {
@@ -16,4 +15,23 @@ test('create a player object', () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   )
+})
+
+test('gets player\'s stats as an object', () => {
+  const player = new Player('Dave')
+
+  expect(player.getStats()).toHaveProperty('potions')
+  expect(player.getStats()).toHaveProperty('health')
+  expect(player.getStats()).toHaveProperty('strength')
+  expect(player.getStats()).toHaveProperty('agility')
+})
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave')
+
+  expect(player.getInventory()).toEqual(expect.any(Array))
+
+  player.inventory = []
+
+  expect(player.getInventory()).toEqual(false)
 })
